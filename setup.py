b2e464be99a7af9083d7134147b9a502456ec2cc
Rahul <rahul.talangkar@gmail.com>
2018-01-09 19:17:09 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+ 
+setup(     
+     name='tenant_users',    # This is the name of your PyPI-package.
+     version='0.1',                          # Update the version number for new releases
+     packages = find_packages()                 # The name of your scipt, and also the command you'll be using for calling it
+ )
+
